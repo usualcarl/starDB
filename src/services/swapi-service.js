923d8e94297aeff6755a4a1e180944c2ab28f664
usualcarl <mlb.chingiz@gmail.com>
2024-04-26 04:25:47 +0500

    добавил hoc хелперы

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -1,5 +1,6 @@
 export default class SwapiService {
-    _apiBase = 'https://swapi.dev/api/';
+    _apiBase = 'https://swapi.co/api';
+    _imageBase = 'https://starwars-visualguide.com/assets/img';
 
     getResource = async (url) => {
         const res = await fetch(this._apiBase + url);
@@ -39,10 +40,16 @@ export default class SwapiService {
         return this._transformStarship(starship)
     };
 
+    getPersonImage = ({id}) => `${this._imageBase}/characters/${id}.jpg`;
+
+    getStarshipImage = ({id}) => `${this._imageBase}/starships/${id}.jpg`;
+
+    getPlanetImage = ({id}) => `${this._imageBase}/planets/${id}.jpg`;
+
     _extractId = (item) => {
         const idRegExp = /\/([0-9]*)\/$/;
         return item.url.match(idRegExp)[1];
-    }
+    };
 
     _transformPlanet = (planet) => {
         return {
@@ -60,11 +67,11 @@ export default class SwapiService {
             name: starship.name,
             model: starship.model,
             manufacturer: starship.manufacturer,
-            costInCredits: starship.costInCredits,
+            costInCredits: starship.cost_in_credits,
             length: starship.length,
             crew: starship.crew,
             passengers: starship.passengers,
-            cargoCapacity: starship.cargoCapacity
+            cargoCapacity: starship.cargo_capacity
         }
     };
 
@@ -74,7 +81,7 @@ export default class SwapiService {
             name: person.name,
             gender: person.gender,
             birthYear: person.birth_year,
-            eyeColor: person.eyeColor
+            eyeColor: person.eye_color
         }
     }
 }
