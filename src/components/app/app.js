1f729ff49c96580d9d96ab903960c68e5de82b0a
usualcarl <mlb.chingiz@gmail.com>
2024-04-26 03:12:17 +0500

    рефактуринг кода 2

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -63,6 +63,7 @@ export default class App extends Component{
                         <ItemList
                             onItemSelected={this.onPersonSelected}
                             getData={this.swapiService.getAllPlanets}
+                            renderItem={item => (<span>{item.name} <button>!</button></span>)}
                         />
                     </div>
                     <div className="col-md-6">
@@ -75,6 +76,7 @@ export default class App extends Component{
                         <ItemList
                             onItemSelected={this.onPersonSelected}
                             getData={this.swapiService.getAllStarships}
+                            renderItem={item => item.name}
                         />
                     </div>
                     <div className="col-md-6">
