f92fe7eff425d600ba7bcb2c65c35dcbec3d9fa8
usualcarl <mlb.chingiz@gmail.com>
2024-04-26 04:51:52 +0500

    рефактуринг

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './item-list.css';
+import PropTypes from 'prop-types';
 
 
 
@@ -26,5 +27,16 @@ const ItemList = (props) => {
         </ul>
     );
 };
+
+ItemList.defaultProps = {
+    onItemSelected: () => {}
+};
+
+ItemList.propTypes = {
+    onItemSelected: PropTypes.func,
+    data: PropTypes.arrayOf(PropTypes.object).isRequired,
+    children: PropTypes.func.isRequired
+};
+
 
 export default ItemList;
